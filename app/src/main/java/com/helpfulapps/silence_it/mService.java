81f43be4ba4a4cbadce201428c7e857a56a68ae7
Karan Bhanot <bhanotkaran22@gmail.com>
2016-03-14 03:51:42 +0530

    Version 1.6

--- app/src/main/java/com/helpfulapps/silence_it/mService.java
+++ app/src/main/java/com/helpfulapps/silence_it/mService.java
@@ -1,17 +1,21 @@
 package com.helpfulapps.silence_it;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.res.Resources;
 import android.media.AudioManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.Chronometer;
 import android.widget.Toast;
@@ -31,25 +35,39 @@ public class mService extends Service {
         if(flag) {
             keeprunning = true;
             Log.i("TAG", "MiddleTop");
-            final int id = 1234;
+            /*final int id = 1234;
             Intent intenti = new Intent(this, MainActivity.class);
             intenti.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             PendingIntent pendIntent = PendingIntent.getActivity(this, 0, intenti, 0);
 
 //Build the notification
-            /*Notification.Builder builder = new Notification.Builder(getBaseContext());
+            Notification.Builder builder = new Notification.Builder(getBaseContext());
             builder.setContentIntent(pendIntent);
             builder.setTicker("Service Started");
             builder.setWhen(System.currentTimeMillis());
             builder.setAutoCancel(false);
             builder.setContentTitle("Silence It");
             builder.setContentText("Touch to disable in Options");
-            //builder.setAutoCancel(true);
 
-            Notification notification = builder.build();
+            Notification notification = builder.build();*/
 
-//start foreground service
-            startForeground(id, notification);*/
+            Intent intenti = new Intent(this, MainActivity.class);
+            intenti.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            PendingIntent pendIntent = PendingIntent.getActivity(this, 0, intenti, 0);
+            //PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+            Resources r = getResources();
+            Notification notification = new NotificationCompat.Builder(getBaseContext())
+                    .setTicker("Start")
+                    .setSmallIcon(R.drawable.main_icon)
+                    .setContentTitle("Silence it")
+                    .setContentText("Silence It is running.")
+                    .setContentIntent(pendIntent)
+                    .setAutoCancel(false)
+                    .setOngoing(true)
+                    .build();
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(1, notification);
 
             Log.i("TAG", "Start");
             new Thread(new Runnable() {
@@ -89,6 +107,8 @@ public class mService extends Service {
     public void onDestroy() {
         super.onDestroy();
         keeprunning = false;
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.cancel(1);
         AudioManager manager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         manager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
         //Toast.makeText(this,"Bye",Toast.LENGTH_LONG).show();
